d862ed1cb51d1cab71654565ca0e5a9ecb966a51
romiyadangol <dangolromiya@gmail.com>
2022-04-03 08:57:19 +0545

    Changes

---
+++ WebApplication2/Controllers/bookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class bookController : Controller
+    {
+        empEntities1 db = new empEntities1();
+        // GET: book
+        public ActionResult book()
+        {
+            List<Table_3> data = db.Table_3.ToList();
+            return View(data);
+        }
+        public ActionResult Edit(int Bid)
+        {
+            Table_3 data = db.Table_3.Find(Bid);//find data using primary key
+            return View(data);
+        }
+
+
+    }
+}
--- WebApplication2/Models/Model1.Context.cs
+++ WebApplication2/Models/Model1.Context.cs
@@ -27,5 +27,6 @@ namespace WebApplication2.Models
     
         public virtual DbSet<Table_1> Table_1 { get; set; }
         public virtual DbSet<Table_2> Table_2 { get; set; }
+        public virtual DbSet<Table_3> Table_3 { get; set; }
     }
 }
