685bce8c5da7b298b820bfe76697ba226a740205
romiyadangol <dangolromiya@gmail.com>
2022-04-03 09:14:36 +0545

    Chaaaange

--- WebApplication2/Controllers/bookController.cs
+++ WebApplication2/Controllers/bookController.cs
@@ -16,12 +16,32 @@ namespace WebApplication2.Controllers
             List<Table_3> data = db.Table_3.ToList();
             return View(data);
         }
+        public ActionResult create()
+        {
+            return View();
+        }
+
         public ActionResult Edit(int Bid)
         {
             Table_3 data = db.Table_3.Find(Bid);//find data using primary key
             return View(data);
         }
 
+        public ActionResult Updatedata(Table_3 table_3)
+        {
+            db.Entry(table_3).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("book");
+        }
+
+
+
+        public ActionResult SaveData(Table_3 table_3)
+        {
+            db.Table_3.Add(table_3);
+            db.SaveChanges();
+            return RedirectToAction("book");
+        }
 
     }
 }
